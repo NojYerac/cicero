16f5b74225c80ae70214a78761375a969a78353c
Noj Yerac <noj.yerac@gmail.com>
2015-04-16 08:34:39 -0500

    karma conf changed for phantom client only

--- karma.conf.js
+++ karma.conf.js
@@ -70,7 +70,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['PhantomJS','Chrome','Firefox'],
+    browsers: ['PhantomJS'/*,'Chrome','Firefox'*/],
 
 
     // Continuous Integration mode
